4a7b7dc90c861d5b5bce024cdb5d461dfa332640
ShehabEAli <shehabelsehelly@gmail.com>
2026-02-18 16:03:56 +0200

    DB and postman requests added

--- ApplicationStructure/src/modules/note/note.controller.js
+++ ApplicationStructure/src/modules/note/note.controller.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { auth } from '../auth/auth.middleware.js';
-import { createNote, updateNote } from './note.service.js';
+import { createNote, deleteNote, replaceNote, UpdateAllTitle, updateNote, getPaginatedNotes, getById, getByContent, getNotesWithUser, deleteAll, aggregateNotes } from './note.service.js';
 const router = Router();
 
 
@@ -9,63 +9,62 @@ router.post("", auth, async (req, res, next) => {
     return res.status(201).json({ message: "Note Created", result })
 })
 
-router.patch("/:noteId", auth, async (req, res, next) => {
-    try {
-        const noteId = req.params.noteId;       
-        const userId = req.userId;             
-        const body = req.body;
-
-        console.log(userId)
-        const updatedNote = await updateNote(noteId, body, userId);
-        res.status(200).json({message: "updated",note: updatedNote});
-    } catch (err) {
-        res.status(400).json({message: err.message});
-    }
-})
-
 router.put("/replace/:noteId", auth, async (req, res, next) => {
-    const result = await ReplaceNote(req.body, req.userId)
+    const result = await replaceNote(req.body, req.params.noteId, req.userId)
     return res.status(201).json(result)
 })
 
 router.patch("/all", auth, async (req, res, next) => {
-    const result = await login(req.body)
+    const result = await UpdateAllTitle(req.body, req.userId);
     return res.status(201).json(result)
+})
+
+router.patch("/:noteId", auth, async (req, res, next) => {
+    try {
+        const noteId = req.params.noteId;
+        const userId = req.userId;
+        const body = req.body;
+
+        console.log(userId)
+        const updatedNote = await updateNote(noteId, body, userId);
+        res.status(200).json({ message: "updated", note: updatedNote });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 })
 
 router.delete("/:noteId", auth, async (req, res, next) => {
-    const result = await login(req.body)
-    return res.status(201).json(result)
+    const result = await deleteNote(req.params.noteId, req.userId)
+    return res.status(201).json({ message: "deleted", result })
 })
 
 router.get("/paginate-sort", auth, async (req, res, next) => {
-    const result = await login(req.body)
-    return res.status(201).json(result)
-})
-
-router.get("/:id", auth, async (req, res, next) => {
-    const result = await login(req.body)
+    const result = await getPaginatedNotes(req.query, req.userId)
     return res.status(201).json(result)
 })
 
 router.get("/note-by-content", auth, async (req, res, next) => {
-    const result = await login(req.body)
+    const result = await getByContent(req.query.content, req.userId)
     return res.status(201).json(result)
 })
 
 router.get("/note-with-user", auth, async (req, res, next) => {
-    const result = await login(req.body)
+    const result = await getNotesWithUser(req.userId)
     return res.status(201).json(result)
 })
 
 router.get("/aggregate", auth, async (req, res, next) => {
-    const result = await login(req.body)
+    const result = await aggregateNotes(req.userId, req.query.title)
     return res.status(201).json(result)
 })
 
 router.delete("", auth, async (req, res, next) => {
-    const result = await login(req.body)
+    const result = await deleteAll(req.body)
     return res.status(201).json(result)
 })
 
+router.get("/:id", auth, async (req, res, next) => {
+    const result = await getById(req.params.id, req.userId)
+    return res.status(201).json(result)
+})
 export default router
--- ApplicationStructure/src/modules/note/note.service.js
+++ ApplicationStructure/src/modules/note/note.service.js
@@ -43,6 +43,162 @@ export const updateNote = async (noteId, body, userId) => {
     return note;
 };
 
-export const ReplaceNote = async (inputs) => {
+export const replaceNote = async (body, _id, userId) => {
+    const { title, content } = body;
 
+    const replaced = await NoteModel.findOneAndReplace(
+        { _id, userId: new mongoose.Types.ObjectId(userId) },
+        {
+            title,
+            content,
+            userId
+        },
+        { new: true }
+    );
+
+    if (!replaced) {
+        throw new Error("Note not found or You are not the owner");
+    }
+
+    return replaced;
+};
+
+export const UpdateAllTitle = async (body, userId) => {
+    const { title } = body;
+    const user = await UserModel.findById(userId);
+    if (!user) {
+        throw new Error("you are not the owner");
+    }
+
+    const notes = await NoteModel.updateMany({}, { $set: { title } })
+    return notes
+}
+
+export const deleteNote = async (_id, userId) => {
+
+    // 2) find note first
+    const note = await NoteModel.findById(_id);
+    if (!note) {
+        throw new Error("Note not found");
+    }
+
+    // 3) ownership check
+    if (note.userId.toString() !== userId.toString()) {
+        throw new Error("You are not the owner");
+    }
+
+    // 4) delete
+    await NoteModel.deleteOne({ _id });
+
+    return { message: "Note deleted successfully" };
+}
+
+export const getPaginatedNotes = async (query, userId) => {
+    let { page = 1, limit = 10 } = query;
+
+    page = Number(page);
+    limit = Number(limit);
+
+    const skip = (page - 1) * limit;
+
+    const Object_userId = new mongoose.Types.ObjectId(userId);
+
+    // const totalNotes = await NoteModel.countDocuments({ userId: Object_userId });
+    // console.log("total notes for user:", totalNotes);
+
+    const notes = await NoteModel.find({ userId: Object_userId })
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit);
+    // console.log("notes returned:", notes);
+    return notes;
+}
+
+export const getById = async (_id, userId) => {
+
+    const note = await NoteModel.findById(_id);
+    if (!note) {
+        throw new Error("Note not found");
+    }
+
+    if (note.userId.toString() !== userId.toString()) {
+        throw new Error("You are not the owner");
+    }
+    return note
+}
+
+export const getByContent = async (content, userId) => {
+
+    const note = await NoteModel.findOne({ content });
+    if (!note) {
+        throw new Error("Note not found");
+    }
+
+    if (note.userId.toString() !== userId.toString()) {
+        throw new Error("You are not the owner");
+    }
+    return note
+}
+
+export const getNotesWithUser = async (userId) => {
+    const notes = await NoteModel.find({ userId })
+        .select("title userId createdAt")
+        .populate({
+            path: "userId",
+            select: "email"
+        });
+
+    return notes;
+}
+
+export const aggregateNotes = async (userId, title) => {
+
+    const matchStage = {
+        userId: new mongoose.Types.ObjectId(userId)
+    };
+
+    if (title) {
+        matchStage.title = { $regex: title, $options: "i" }; 
+    }
+
+    const notes = await NoteModel.aggregate([
+
+        { $match: matchStage },
+
+        {
+            $lookup: {
+                from: "users",
+                localField: "userId",
+                foreignField: "_id",
+                as: "user"
+            }
+        },
+
+        { $unwind: "$user" },
+
+        {
+            $project: {
+                title: 1,
+                userId: 1,
+                createdAt: 1,
+                "user.name": 1,
+                "user.email": 1
+            }
+        }
+
+    ]);
+
+    return notes;
+};
+
+export const deleteAll = async (userId) => {
+
+    const result = await NoteModel.deleteMany(userId);
+    console.log(result);
+
+    if (result.deletedCount === 0) {
+        throw new Error("You don't have any notes");
+    }
+
+    return { message: "Notes deleted successfully" };
 }
